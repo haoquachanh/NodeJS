d3464480c00127c0003f55292d365e202b76e16d
haoquachanh <anhhaomnmm@gmail.com>
2022-07-24 04:53:19 +0700

    ejs, mvc

---
+++ src/route/web.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import homeController from '../controller/homeController'
+
+let router = express.Router()
+
+const initWebRoute = (app) => {
+    router.get('/', homeController.getHomePage)
+    router.get('/about',(req,res)=> {
+        res.send(`I'm here for you, and you?`)
+    })
+    return app.use('/',router)
+}
+
+export default initWebRoute
--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 const { render } = require('ejs');
 const express =require('express');
 const { default: configViewEngine } = require('./configs/viewEngine');
-
+const { default: initWebRoute } = require('./route/web');
+require('dotenv').config(); 
     const app=express();
-    const port=3001;
+    const port=process.env.PORT || 3001;
 
+    // setup view engine
     configViewEngine(app)
-    app.get('/',(req,res)=> {
-        res.render('index.ejs')
-    })
 
+    //init web route 
+    initWebRoute(app)
     app.listen(port,()=>{
         console.log(`Port: ${port}`)
     })
