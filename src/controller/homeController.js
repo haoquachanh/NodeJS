ba88c2c5d289111e3af01e45eaddecad623f0d44
haoquachanh <anhhaomnmm@gmail.com>
2022-09-14 02:50:06 +0700

    full CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -34,9 +34,33 @@ let getCreateNewUser = async (req,res)=>{
     let {firstName,lastName,email,address}=req.body;
     await pool.execute(`insert into users(firstName, lastName, email, address) values(?,?,?,?)`,[firstName, lastName, email, address])
 
-    return res.send('Added to DB')
+    return res.redirect('/')
 }
 
+let deleteUser = async(req,res) => {
+    // res.send(`ok ?? === ${req.body.userId}`)
+    let userId=req.body.userId;
+    await pool.execute('delete from users where id=?',[userId])
+    return res.redirect('/');
+}
+
+let editUser=async(req,res)=>{
+    // res.send(`ok ? = ${req.params.iduser}`)
+    let idd=req.params.iduser;
+    
+    // console.log(req.params.iduser)
+    // console.log(idd)/
+    let [user]= await pool.execute('select * from users where id=?',[idd])
+
+    return res.render('update.ejs',{data: user[0]})  
+}
+
+let postUpdateUser=async(req,res)=>{
+    let {firstName,lastName,email,address,id} = req.body
+    // console.log(req.body)
+    await pool.execute('update users set firstName = ?, lastName=?, email=?, address=? where id=?',[firstName,lastName,email,address,id])
+    res.redirect('/')
+}
 module.exports = {
-    getHomePage, getDetailPage, getCreateNewUser
+    getHomePage, getDetailPage, getCreateNewUser,deleteUser,editUser,postUpdateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,9 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage)
     router.post('/create-new-user', homeController.getCreateNewUser)
     router.get('/detail/user/:userId', homeController.getDetailPage)
+    router.get('/edit-user/:iduser',homeController.editUser)
+    router.post('/delete-user',homeController.deleteUser)
+    router.post('/post-update',homeController.postUpdateUser)
     router.get('/about',(req,res)=> {
         res.send(`I'm here for you, and you?`)
     })
